1fdfd61a2986f0d76e9669233640a81b58fb1a6d
Justin Bell <Juxin203@gmail.com>
2024-09-30 16:15:21 -0600

    init

---
+++ ShuffletheDeck/ShuffletheDeck.vb
@@ -0,0 +1,16 @@
+﻿'Justin Bell
+'RCET0265
+'Fall 2024
+'Shuffle the Deck
+' link
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+Module ShuffletheDeck
+
+    Sub Main()
+
+    End Sub
+
+End Module
